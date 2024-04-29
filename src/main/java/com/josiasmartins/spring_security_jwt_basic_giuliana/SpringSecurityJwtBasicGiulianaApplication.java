c53bb3a93558116824e4c9cf482b44d5cd627b56
Caitano <josias.m.caitano@accenture.com>
2024-04-29 10:57:34 -0300

    feature: initial project

---
+++ src/main/java/com/josiasmartins/spring_security_jwt_basic_giuliana/SpringSecurityJwtBasicGiulianaApplication.java
@@ -0,0 +1,13 @@
+package com.josiasmartins.spring_security_jwt_basic_giuliana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtBasicGiulianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtBasicGiulianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josiasmartins/spring_security_jwt_basic_giuliana/SpringSecurityJwtBasicGiulianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiasmartins.spring_security_jwt_basic_giuliana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtBasicGiulianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
